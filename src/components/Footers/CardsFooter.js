57d14f1684da8a39ebb2f7104d8469746384e8db
MauricioSilvaSantos <mauricio_duality@hotmail.com>
2021-09-17 20:11:11 -0400

    test

--- src/components/Footers/CardsFooter.js
+++ src/components/Footers/CardsFooter.js
@@ -9,7 +9,6 @@
 * Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
 =========================================================
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
 /*eslint-disable*/
